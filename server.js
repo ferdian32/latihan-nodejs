6197069b1ba330c7d96491003f22c2aaec3b6d5e
=ferdian32 <fierliferdiansyah0@gmail.com>
2025-05-21 20:06:35 +0700

    membuat status response node js

--- server.js
+++ server.js
@@ -1,49 +1,51 @@
 const http = require("http");
 const { buffer } = require("stream/consumers");
 
-const HttpListener = (request, response) => {
+const requestListener = (request, response) => {
+  response.setHeader("Content-Type", "text/html");
+
   const { method, url } = request;
-  let body = [];
-  request.on("data", (chunk) => {
-    body.push(chunk);
-  });
-  request.on("end", () => {
-    body = Buffer.concat(body).toString();
-  });
+
   if (url === "/") {
     if (method === "GET") {
-      console.log("<h1>Ini adalah halaman home page</h1>");
+      response.statusCode = 200;
+      response.end("<h1>Ini adalah homepage</h1>");
     } else {
+      response.statusCode = 400;
       response.end(
-        `<h1>Halaman tidak dapat diakses dengan method ${method}</h1>`
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
       );
     }
   } else if (url === "/about") {
     if (method === "GET") {
-      response.end("<h1>Ini adalah halaman about page</h1>");
+      response.statusCode = 200;
+      response.end("<h1>Halo! Ini adalah halaman about</h1>");
     } else if (method === "POST") {
       let body = [];
+
       request.on("data", (chunk) => {
         body.push(chunk);
       });
+
       request.on("end", () => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
-        response.end(`<h1>Halo ${name}</h1>`);
+        response.statusCode = 200;
+        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
       });
-      response.end("<h1>Halaman ini merupakan halaman about</h1>");
     } else {
-      response.end(`tidak dapat ditemukan dengan ${method}`);
+      response.statusCode = 400;
+      response.end(
+        `<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`
+      );
     }
   } else {
-    response.end("<h1>Halaman Tidak Ditemukan</h1>");
+    response.statusCode = 404;
+    response.end("<h1>Halaman tidak ditemukan!</h1>");
   }
-  response.setHeader("Content-Type", "text/html");
-  response.statusCode = 200;
-  //   response.end("<h1>Halo HTTP Server</h1>");
 };
 
-const server = http.createServer(HttpListener);
+const server = http.createServer(requestListener);
 const port = 5000;
 const host = "localhost";
 server.listen(port, host, () => {
