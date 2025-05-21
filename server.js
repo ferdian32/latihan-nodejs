139c25d337a5e859552b5ee6eece2b310a6fc512
=ferdian32 <fierliferdiansyah0@gmail.com>
2025-05-21 19:35:23 +0700

    membuat routing request menggunakan node js

--- server.js
+++ server.js
@@ -1,26 +1,50 @@
 const http = require("http");
+const { buffer } = require("stream/consumers");
 
 const HttpListener = (request, response) => {
-  const { method } = request;
-  if (method === "GET") {
-    console.log("<h1>Hai</h1>");
-  }
-  if (method === "POST") {
-    console.log("<h1>Halo</h1>");
-  }
-  if (method === "PUT") {
-    console.log("<h1>Bonjour!</h1>");
-  }
-  if (method === "DELETE") {
-    response.end("<h1>Salam</h1>");
+  const { method, url } = request;
+  let body = [];
+  request.on("data", (chunk) => {
+    body.push(chunk);
+  });
+  request.on("end", () => {
+    body = Buffer.concat(body).toString();
+  });
+  if (url === "/") {
+    if (method === "GET") {
+      console.log("<h1>Ini adalah halaman home page</h1>");
+    } else {
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan method ${method}</h1>`
+      );
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.end("<h1>Ini adalah halaman about page</h1>");
+    } else if (method === "POST") {
+      let body = [];
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Halo ${name}</h1>`);
+      });
+      response.end("<h1>Halaman ini merupakan halaman about</h1>");
+    } else {
+      response.end(`tidak dapat ditemukan dengan ${method}`);
+    }
+  } else {
+    response.end("<h1>Halaman Tidak Ditemukan</h1>");
   }
   response.setHeader("Content-Type", "text/html");
   response.statusCode = 200;
-  response.end("<h1>Halo HTTP Server</h1>");
+  //   response.end("<h1>Halo HTTP Server</h1>");
 };
 
 const server = http.createServer(HttpListener);
-const port = 3000;
+const port = 5000;
 const host = "localhost";
 server.listen(port, host, () => {
   console.log(`Server Berjalan pada ${host}:${port}`);
